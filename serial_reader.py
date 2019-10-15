3e81f5afa568728324ae013ff29a53b74e2ac5d1
Mark Parris <mparris@chefsteps.com>
2019-10-15 10:43:46 -0700

    Don't print debug output by default

--- serial_reader.py
+++ serial_reader.py
@@ -13,6 +13,9 @@ def run(device, baud, prefix=None):
                 continue
             if prefix:
                 line = prefix() + line
+            if (line.find('DEBUG: STREAM: DEBUG: - ') > 0) and not (args.logDebug == True):
+                continue
+
             sys.stdout.write(line)
 
 if __name__ == '__main__':
@@ -20,6 +23,7 @@ if __name__ == '__main__':
     parser.add_argument('device',
                         help='serial device, typically /dev/tty.usbserial-*')
     parser.add_argument('--baud', dest='baud', type=int, default=74880)
+    parser.add_argument('-d', dest='logDebug', action='store_true')
     parser.add_argument('-t', '--timestamp', dest='timestamp', action='store_true',
                         help="Add timestamp to start of each line")
     args = parser.parse_args()
